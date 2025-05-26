c9e0dad2da4a1fec0110aef7a899df2109061e56
[CLOUD4] <dbslxl11@gmail.com>
2025-05-26 22:56:51 +0900

    refactor: 코드레빗 설정 변경

--- src/main/java/com/example/admin_project/menu/service/MenuService.java
+++ src/main/java/com/example/admin_project/menu/service/MenuService.java
@@ -14,11 +14,7 @@ import java.util.List;
 public class MenuService {
     private final MenuRepository menuRepository;
     private final MenuMapper menuMapper;
-    /**
-     * Retrieves all menu items and returns them as a list of MenuResponse DTOs.
-     *
-     * @return a list of MenuResponse objects representing all menus
-     */
+
     public List<MenuResponse> findMenuList() {
         List<Menu> menuEntity = menuRepository.findAll();
         return menuMapper.toDtoList(menuEntity);
