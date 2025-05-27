27a0293130eeb121f3322f2b1b1aaf4266613d44
[CLOUD4] <dbslxl11@gmail.com>
2025-05-27 22:21:01 +0900

    feat: 메뉴 관리 추가 기능 구현

--- src/main/java/com/example/admin_project/menu/service/MenuService.java
+++ src/main/java/com/example/admin_project/menu/service/MenuService.java
@@ -1,7 +1,9 @@
 package com.example.admin_project.menu.service;
 
+import com.example.admin_project.menu.dto.MenuCreateRequest;
 import com.example.admin_project.menu.dto.MenuResponse;
 import com.example.admin_project.menu.entity.Menu;
+import com.example.admin_project.menu.exception.MenuNotFoundException;
 import com.example.admin_project.menu.mapper.MenuMapper;
 import com.example.admin_project.menu.repository.MenuRepository;
 import lombok.AllArgsConstructor;
@@ -22,4 +24,13 @@ public class MenuService {
         return menuMapper.toDtoList(menuEntity);
     }
 
+    public void createMenu(MenuCreateRequest menuCreateRequest) {
+        Menu parentMenu = null;
+        if(menuCreateRequest.getParentId() != null) {
+            parentMenu = menuRepository.findById(menuCreateRequest.getParentId())
+                    .orElseThrow(() -> new MenuNotFoundException("부모 메뉴가 존재하지 않습니다."));
+        }
+        Menu menuEntity = menuCreateRequest.toEntity(parentMenu);
+        menuRepository.save(menuEntity);
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,19 @@
 -- 상위 메뉴 1
-INSERT INTO menu (id, menu_name, sort_order, is_use, parent_menu_id)
-VALUES (1, '대시보드', 1, true, NULL);
+INSERT INTO menu (menu_name, sort_order, is_use, parent_menu_id)
+VALUES ('대시보드', 1, true, NULL);
 
 -- 상위 메뉴 2
-INSERT INTO menu (id, menu_name, sort_order, is_use, parent_menu_id)
-VALUES (2, '관리자 설정', 2, true, NULL);
+INSERT INTO menu (menu_name, sort_order, is_use, parent_menu_id)
+VALUES ('관리자 설정', 2, true, NULL);
 
 -- 하위 메뉴 1 (대시보드 하위)
-INSERT INTO menu (id, menu_name, sort_order, is_use, parent_menu_id)
-VALUES (3, '통계 보기', 1, true, 1);
+INSERT INTO menu (menu_name, sort_order, is_use, parent_menu_id)
+VALUES ('통계 보기', 1, true, 1);
 
 -- 하위 메뉴 2 (대시보드 하위)
-INSERT INTO menu (id, menu_name, sort_order, is_use, parent_menu_id)
-VALUES (4, '로그 확인', 2, true, 1);
+INSERT INTO menu (menu_name, sort_order, is_use, parent_menu_id)
+VALUES ('로그 확인', 2, true, 1);
 
 -- 하위 메뉴 3 (관리자 설정 하위)
-INSERT INTO menu (id, menu_name, sort_order, is_use, parent_menu_id)
-VALUES (5, '권한 관리', 1, true, 2);
+INSERT INTO menu (menu_name, sort_order, is_use, parent_menu_id)
+VALUES ('권한 관리', 1, true, 2);
