853f4b3af032ca1d6240d88a81ba65759b2c3ee2
[CLOUD4] <dbslxl11@gmail.com>
2025-05-28 11:07:08 +0900

    feat: 메뉴 관리 유효성 검증 로직 추가 1. 자식 메뉴 순서, 메뉴 이름 2. 부모 메뉴 순서, 메뉴 이름

---
+++ src/main/java/com/example/admin_project/menu/exception/DuplicateChildrenMenuNameException.java
@@ -0,0 +1,8 @@
+package com.example.admin_project.menu.exception;
+
+
+public class DuplicateChildrenMenuNameException extends RuntimeException {
+    public DuplicateChildrenMenuNameException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/admin_project/menu/exception/DuplicateChildrenSortOrderException.java
@@ -0,0 +1,8 @@
+package com.example.admin_project.menu.exception;
+
+
+public class DuplicateChildrenSortOrderException extends RuntimeException {
+    public DuplicateChildrenSortOrderException(String message) {
+        super(message);
+    }
+}
